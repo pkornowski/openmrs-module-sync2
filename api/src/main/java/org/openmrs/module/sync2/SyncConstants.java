3df5747534d760388a7c0d4e1631b84de792def6
Przemyslaw Kornowski <pkornowski@soldevelo.com>
2018-01-15 15:15:14 +0100

    SYNCT-75: Fixed issue with synchronization loop, during object update operation. (REST)

--- api/src/main/java/org/openmrs/module/sync2/SyncConstants.java
+++ api/src/main/java/org/openmrs/module/sync2/SyncConstants.java
@@ -39,4 +39,10 @@ public class SyncConstants {
 
     public static final String ACTION_CREATED = "CREATED";
 
+    public static final String CATEGORY_PATIENT = "patient";
+
+    public static final String CATEGORY_LOCATION = "location";
+
+    public static final String CATEGORY_PRIVILEGE = "privilege";
+
 }
